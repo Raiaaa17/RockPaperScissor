44989e4930881232cb1acc456843b12c041c1509
raiaaa17 <rayanarahadiva@gmail.com>
2024-09-22 01:02:49 +0800

    Finish Rock Paper Scissor

--- myScript.js
+++ myScript.js
@@ -1,5 +1,6 @@
 console.log("Rock Papper Scissor!")
 
+// computer hand function
 function getComputerChoice() {
     let a = Math.random();
     // console.log(a)
@@ -12,11 +13,49 @@ function getComputerChoice() {
     }
 }
 
+// human hand function
 function getHumanChoice() {
     let b = prompt("Choose your hand! ");
-    return ("you choose" + b);
+    b = b.toLowerCase();
+    if (b === "rock") {
+        return ("rock");
+    } else if (b === "scissor") {
+        return ("scissor");
+    } else if (b === "paper") {
+        return ("paper");
+    } else {
+        getHumanChoice();
+    }
 }
 
-// console.log(getComputerChoice())
+// initialize score
+let humanScore = 0;
+let computerScore = 0;
 
-console.log(getHumanChoice())
+
+// round function
+function playRound(humanHand, computerHand) {
+    console.log(`computer: ${computerHand}`)
+    console.log(`human: ${humanHand}`)
+
+    if (humanHand === computerHand) {
+        console.log(`both you and computer choose ${humanHand}. It's a tie!`)
+    } else if ((humanHand === "paper" & computerHand === "rock") 
+        || (humanHand === "rock" & computerHand === "scissor")
+        || (humanHand === "scissor" & computerHand === "paper")){
+        console.log(`you choose ${humanHand} and computer choose ${computerHand}. You win!`)
+        humanScore += 1
+    } else {
+        console.log(`you choose ${humanHand} and computer choose ${computerHand}. You lose!`)
+        computerScore += 1
+    }
+    
+}
+
+
+// play Rock Papper Scissor! 
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
+console.log(`human score: ${humanScore}, computer score: ${computerScore}`)
